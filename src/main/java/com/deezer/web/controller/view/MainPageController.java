caf1b8efc33270be991ad38a63daa35273cfb943
Yevheniia <eugeniadzytsiuk@ukr.net>
2018-12-04 17:07:24 +0200

    sonar fix

--- src/main/java/com/deezer/web/controller/view/MainPageController.java
+++ src/main/java/com/deezer/web/controller/view/MainPageController.java
@@ -2,10 +2,8 @@ package com.deezer.web.controller.view;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
